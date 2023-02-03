2541a18de1606137840641aabc709b19de14458f
Andrew Kelley <andrew@ziglang.org>
2023-02-03 16:20:19 -0700

    build.zig: depend on external cf package

--- build.zig
+++ build.zig
@@ -4,13 +4,20 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const cf_pkg = b.dependency("cf", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const cf_module = cf_pkg.module("cf");
+
     const exe = b.addExecutable(.{
         .name = "jazmin",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    //exe.addPackagePath("cf", "dep/cf/cf.zig");
+    exe.addModule("cf", cf_module);
     exe.install();
 
     const run_cmd = exe.run();
@@ -27,7 +34,7 @@ pub fn build(b: *std.build.Builder) void {
         .target = target,
         .optimize = optimize,
     });
-    //exe_tests.addPackagePath("cf", "dep/cf/cf.zig");
+    exe_tests.addModule("cf", cf_module);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
